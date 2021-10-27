3b6baca61bf8d86bb3faf9d2cd1d1aa3bc3cc30f
CamShoward <68672700+CamShoward@users.noreply.github.com>
2021-10-27 22:36:07 +0000

    added support for RE.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import re
 client = discord.Client()
 
 @client.event
@@ -14,4 +15,19 @@ async def on_message(message):
 		emoji = "👍🏿"
 		await message.add_reaction(emoji)
 
-client.run('')
+@client.event
+async def on_message(message):
+	if message.author == client.user:
+		return
+
+	pattern_re = re.compile(r'grinding', re.IGNORECASE)
+	pattern_re2 = re.compile(r'grind', re.IGNORECASE)
+	if re.search(pattern_re, message.content):
+		await message.add_reaction('👍🏿')
+		await message.channel.send("The grind doesn't stop!")
+
+	if re.search(pattern_re2,message.content):
+		await message.add_reaction('👍🏿')
+		await message.channel.send("The grind doesn't stop!")
+
+client.run('ODk3OTY4OTAwNTQ3OTU2NzQ2.YWdYtQ.x9E748wKq8HpjU0w2AE5ABIqC6Y')
